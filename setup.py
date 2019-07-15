cbb16cf69c21c510a2ba75f4d67d48a753c3897d
Lin <wlin25@ur.rochester.edu>
2019-07-15 10:03:41 -0400

    firtst commit; stacks

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = 'pythonds9',
+    version = '0.0.0',
+    author = 'Gracie Lin',
+    author_email = 'wlin25@ur.rochester.edu',
+    description = 'stuff for python algs and data structures',
+    url = 'https://github.com/wlin25/pythonds9',
+    packages = setuptools.find_packages()
+    )
+    
